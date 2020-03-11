e5858fa6a84ff6dda01316c02fb621a0dfb5d670
Leo <leo.80@protonmail.com>
2020-03-11 12:43:45 -0300

    Arrumando a lógica do acesso a página 'deletion-confirmation.php'

--- deletion-confirmation.php
+++ deletion-confirmation.php
@@ -1,9 +1,19 @@
 <?php
+require_once "config.php";
 include "src/templates/header.php";
 session_start();
+$id = $_SESSION['id'];
 
 if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-  session_destroy();
+  $sql = "SELECT * FROM usuarios WHERE id = $id";
+  $stmt = $pdo->query($sql);
+
+  if ($stmt->rowCount() > 0) {
+    header("Location: index.php");
+  } else {
+    session_destroy();
+  }
+
 } else {
   header("Location: index.php");
 }
